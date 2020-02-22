d0b3ecaca32f9c30b02fc269a5d897038e09fbd0
Joseph Allen <joseph.allen@mines.sdsmt.edu>
2020-02-22 12:27:49 -0700

    Added changes to kiwi drive and the example i2c master code.

--- nrc-basic-kiwi-drive/nrc-basic-kiwi-drive.ino
+++ nrc-basic-kiwi-drive/nrc-basic-kiwi-drive.ino
@@ -29,6 +29,7 @@ PS3BT PS3(&Btd);
 
 int x, y, xp, yp, r;
 int w1s, w2s, w3s;
+int w1sOld, w2sOld, w3sOld;
 
 void setup() {
   if (Usb.Init() == -1) {
@@ -49,15 +50,47 @@ void setup() {
   Serial.begin(115200);
 
   Wire.begin();
+
+  //if (!mag.init())
+  //{
+  //    Serial.println("Failed to detect and initialize magnetometer!");
+  //    while (1);
+  //}
+  //mag.enableDefault();
+  
+  //for(int i = 0; i < 5; i++)
+  //{
+  //    mag.read();
+  //    float angle = atan2(x, y) + PI;
+  //    avg += angle;
+  //    delay(100);
+  //}
+  //avg = avg / 5;
+  //setpoint = avg;
 }
 
 void loop() {
   Usb.Task();
   if (PS3.PS3Connected)
   {
+    //grab the analog value from the joystick and run it through the
+    //  joyToPWM function to translate it to something usefull.
+    //x = joyToPWM(PS3.getAnalogHat(RightHatX));
+    //y = joyToPWM(PS3.getAnalogHat(RightHatY));
     xp = joyToPWM(PS3.getAnalogHat(LeftHatX));
     yp = -1 * joyToPWM(PS3.getAnalogHat(LeftHatY));
     r = joyToPWM(PS3.getAnalogHat(RightHatX));
+
+    //mag.read();
+    //float mag_x = mag.m.x - 5850;
+    //float mag_y = mag.m.y + 6750;
+    //float angle = atan2(mag_x, mag_y) + PI;      
+    //theta = setpoint - angle;
+    //if(theta < 0)
+    //theta = theta + 2 * PI;
+       
+    //x = xp*sin(theta) + yp*cos(theta);
+    //y = xp*cos(theta) - yp*sin(theta);
     
     //set the speed and direction of the motors using the L289N library
     w1s = -0.5 * xp - sqrt(3)/2 * yp + r;
@@ -65,28 +98,37 @@ void loop() {
     w3s = xp + r;
 
     //limit speed
-    w1s *= 0.35;
-    w2s *= 0.35;
-    w3s *= 0.35;
+    w1s *= 8;
+    w2s =0; //*= 0.35;
+    w3s =0; //*= 0.35;
 
-    Serial.print(w1s);
-    Serial.print(" ");
-    Serial.print(w2s);
-    Serial.print(" ");
-    Serial.print(w3s);
-    Serial.println();
-        
-    if(w1s < 0)
+
+    if(w1sOld != w1s)
     {
-      digitalWrite(LEN1,HIGH);
-      digitalWrite(REN1,LOW);
-      analogWrite(PWM1,-1*w1s);
-    }
-    else
-    {
-      digitalWrite(REN1,LOW);
-      digitalWrite(LEN1,HIGH);
-      analogWrite(PWM1,w1s);
+      Serial.print(w1s);
+      Serial.print(" ");
+      Serial.println();
+      w1sOld = w1s;
+      if(w1s < 0)
+      {
+        //digitalWrite(LEN1,HIGH);
+        //digitalWrite(REN1,LOW);
+        //analogWrite(PWM1,-1*w1s);
+        Wire.beginTransmission(1); // transmit to device #1      // sends five bytes
+        Wire.write(-1*w1s);              // sends one byte  
+        Wire.write((-1*w1s)>>8);  
+        Wire.endTransmission();    // stop transmitting
+      }
+      else
+      {
+        //digitalWrite(REN1,LOW);
+        //digitalWrite(LEN1,HIGH);
+        //analogWrite(PWM1,w1s);
+        Wire.beginTransmission(1); // transmit to device #1      // sends five bytes
+        Wire.write(-1*w1s);              // sends one byte  
+        Wire.write((-1*w1s)>>8);  
+        Wire.endTransmission();    // stop transmitting
+      }
     }
     
     if(w2s < 0)
@@ -114,6 +156,11 @@ void loop() {
       digitalWrite(LEN3,HIGH);
       analogWrite(PWM3,w3s);
     }
+
+    //Serial.print(w2s);
+    //Serial.print(" ");
+    //Serial.print(w3s);
+    //Serial.println();
     delay(10);
   }
   analogWrite(11,0);
