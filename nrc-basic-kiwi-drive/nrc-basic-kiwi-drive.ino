5d231cad5a842e9a3ee85abd758b6f72c61b4f81
Joseph Allen <joseph.allen@mines.sdsmt.edu>
2020-03-07 15:42:35 -0700

    updated code to be more efficient. Improved magnetometer filtering.

--- nrc-basic-kiwi-drive/nrc-basic-kiwi-drive.ino
+++ nrc-basic-kiwi-drive/nrc-basic-kiwi-drive.ino
@@ -4,158 +4,167 @@
 #include <LIS3MDL.h>
 #include <math.h>
 
-float avg = 0;
-float setpoint, theta;
+const float m1Offset = ( (2*PI)/3);
+const float m2Offset = ( (4*PI)/3);
 
 LIS3MDL mag;
 USB Usb;
 BTD Btd(&Usb);
 PS3BT PS3(&Btd);
 
-int xp, yp, r;
-int w1s, w2s, w3s;
-int w1sOld, w2sOld, w3sOld;
 unsigned long lastUpdate = 0;
+unsigned long timeDiff;
+
 float maxx = 0, maxy =0 , minx =0, miny=0 ;
+
+float setpoint, theta;
+
 void setup() {
-  if (Usb.Init() == -1) {
-    while (1); //halt
-  }
-  // put your setup code here, to run once:
-  
+
   Serial.begin(115200);
 
   Wire.begin();
+  
+  if (Usb.Init() == -1) 
+  {
+      while (true)
+      {
+        //Serial.println("Failed to detect and initialize USB!");
+        delay(500);
+      }
+  }
 
   if (!mag.init())
   {
-      Serial.println("Failed to detect and initialize magnetometer!");
-      while (1);
+      while (true)
+      {
+        //Serial.println("Failed to detect and initialize magnetometer!");
+        delay(500);
+      }
   }
+  
   mag.enableDefault();
   
-  for(int i = 0; i < 5; i++)
+  float avg = 0;
+  
+  for(int i = 0; i < 60; i++)
   {
     mag.read();
     float mag_x = mag.m.x + 6501;// - 5850;
     float mag_y = mag.m.y; + 4753;// + 6750;
-    float angle = atan2(mag_x, mag_y) + PI; 
+    float angle = atan2(mag_x, mag_y); 
     avg += angle;
-    delay(100);
+    delay(50);
+    //Serial.println("test");
   }
-  avg = avg / 5;
+  avg = avg / 60;
   setpoint = avg;
 }
 
 void loop() {
+  int xp, yp, r;
+  int w1s, w2s, w3s;
   Usb.Task();
+  
   if (PS3.PS3Connected)
   {
+    
     //grab the analog value from the joystick and run it through the
-    //  joyToPWM function to translate it to something usefull.
-    //x = joyToPWM(PS3.getAnalogHat(RightHatX));
-    //y = joyToPWM(PS3.getAnalogHat(RightHatY));
+    //joyToPWM function to translate it to something usefull.
     xp = joyToPWM(PS3.getAnalogHat(LeftHatX));
     yp = -1 * joyToPWM(PS3.getAnalogHat(LeftHatY));
     r = joyToPWM(PS3.getAnalogHat(RightHatX));
 
     mag.read();
-    float mag_x = mag.m.x + 6501;
-    float mag_y = mag.m.y + 4753;
-    //maxx = max(maxx,mag_x);
-    //maxy = max(maxy,mag_y);
-    //minx = min(minx,mag_x);
-    //miny = min(miny,mag_y);
+    float mag_x = mag.m.x;
+    float mag_y = mag.m.y;
+
+
+    maxx = max(maxx,mag_x);
+    maxy = max(maxy,mag_y);
+    minx = min(minx,mag_x);
+    miny = min(miny,mag_y);
+
+    mag_x = (mag_x-(minx + maxx)/2.0)/(maxx-minx);
+    mag_y = (mag_y-(miny + maxy)/2.0)/(maxy-miny);
+
+    //debug
+    //Serial.println("X");
     //Serial.println(maxx);
     //Serial.println(minx);
+    //Serial.println("Y");
     //Serial.println(maxy);
     //Serial.println(miny);
-    Serial.print(mag.m.x);
-    Serial.print(',');
-    Serial.println(mag.m.y);
 
-    float angle = atan2(mag_x, mag_y) + PI;      
-    theta = setpoint - angle;
-    if(theta < 0)
-    theta = theta + 2 * PI;
-       
-    xp = xp*sin(theta) + yp*cos(theta);
-    yp = xp*cos(theta) - yp*sin(theta);
+    //Serial.print(mag_x);
+    //Serial.print(",");
+    //Serial.println(mag_y);
     
-    //Serial.println(theta);
+    //compute angle diff betwean current
+    theta = atan2(mag_x, mag_y) - setpoint;
+    if(theta < 0)
+      theta = theta + 2 * PI;
+    
+    //Serial.println(theta*200);
 
-    //set the speed and direction of the motors using the L289N library
-    w1s = -0.5 * xp - sqrt(3)/2 * yp + r;
-    w2s = -0.5 * xp + sqrt(3)/2 * yp + r;
-    w3s = xp + r;
+    //set the velocity of the motors
+    w1s = r + sin(theta - m2Offset)*yp - cos(theta-m2Offset)*xp;
+    w2s = r + sin(theta - m1Offset)*yp - cos(theta-m1Offset)*xp;
+    w3s = r + sin(theta)*yp - cos(theta)*xp;
 
     //limit speed
     w1s *= 1.5;    
     w2s *= 1.5; 
     w3s *= 1.5;
-    
-    if(w1sOld != w1s)
-    {
-      w1sOld = w1s;
-      lastUpdate = millis();
-      Wire.beginTransmission(1); // transmit to device #1      // sends five bytes
-      Wire.write(w1s);              // sends one byte  
-      Wire.write((w1s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-    }
-    
-    if(w2sOld != w2s)
-    {
-      w2sOld = w2s;
-      lastUpdate = millis();
-      Wire.beginTransmission(2); // transmit to device #2      // sends five bytes
-      Wire.write(w2s);              // sends one byte  
-      Wire.write((w2s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-    }
-  
-    if(w3sOld != w3s)
-    {
-      w3sOld = w3s;
-      lastUpdate = millis();
-      Wire.beginTransmission(3); // transmit to device #3      // sends five bytes
-      Wire.write(w3s);              // sends one byte  
-      Wire.write((w3s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-    }
 
-    if((millis() - lastUpdate) > 100)
-    {
-      Wire.beginTransmission(1); // transmit to device #3      // sends five bytes
-      Wire.write(w1s);              // sends one byte  
-      Wire.write((w1s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-      Wire.beginTransmission(2); // transmit to device #3      // sends five bytes
-      Wire.write(w2s);              // sends one byte  
-      Wire.write((w2s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-      Wire.beginTransmission(3); // transmit to device #3      // sends five bytes
-      Wire.write(w3s);              // sends one byte  
-      Wire.write((w3s)>>8);  
-      Wire.endTransmission();    // stop transmitting
-    }
+    //motor 1
+    Wire.beginTransmission(1); // transmit to device #3      // sends five bytes
+    Wire.write(w1s);              // sends one byte  
+    Wire.write((w1s)>>8);  
+    Wire.endTransmission();    // stop transmitting
 
-    delay(10);
+    //motor 2
+    Wire.beginTransmission(2); // transmit to device #3      // sends five bytes
+    Wire.write(w2s);              // sends one byte  
+    Wire.write((w2s)>>8);  
+    Wire.endTransmission();    // stop transmitting
+
+    //motor 3
+    Wire.beginTransmission(3); // transmit to device #3      // sends five bytes
+    Wire.write(w3s);              // sends one byte  
+    Wire.write((w3s)>>8);  
+    Wire.endTransmission();    // stop transmitting
+
+    //debug
+    Serial.print(theta*63.66);
+    Serial.print(',');
+    Serial.print(w1s);
+    Serial.print(',');
+    Serial.print(w2s);
+    Serial.print(',');
+    Serial.println(w3s);
+//    
+//    theta += (timeDiff*0.0418879023)/100;
+//    if(theta > (2*PI))
+//      theta -= 2*PI;
+//    delay(10);
+      
   }
   else // set speed to zero if controller is not connected
   {
-    Wire.beginTransmission(1); // transmit to device #1      // sends five bytes
-    Wire.write(0);              // sends one byte  
-    Wire.write((0)>>8);  
-    Wire.endTransmission();    // stop transmitting
-    Wire.beginTransmission(2); // transmit to device #2      // sends five bytes
-    Wire.write(0);              // sends one byte  
-    Wire.write((0)>>8);  
-    Wire.endTransmission();    // stop transmitting
-    Wire.beginTransmission(3); // transmit to device #3      // sends five bytes
-    Wire.write(0);              // sends one byte  
-    Wire.write((0)>>8);  
-    Wire.endTransmission();    // stop transmitting
+    //Wire.beginTransmission(1); // transmit to device #1      // sends five bytes
+    //Wire.write(0);              // sends one byte  
+    //Wire.write((0)>>8);  
+    //Wire.endTransmission();    // stop transmitting
+    //Wire.beginTransmission(2); // transmit to device #2      // sends five bytes
+    //Wire.write(0);              // sends one byte  
+    //Wire.write((0)>>8);  
+    //Wire.endTransmission();    // stop transmitting
+    //Wire.beginTransmission(3); // transmit to device #3      // sends five bytes
+    //Wire.write(0);              // sends one byte  
+    //Wire.write((0)>>8);  
+    //Wire.endTransmission();    // stop transmitting
+    //Serial.println("Failed to detect PS3.");
   }
 }
 
